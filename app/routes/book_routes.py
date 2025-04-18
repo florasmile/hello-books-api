b7f9984f5c0f25c3d59ac77e3ece4d8f2af2abb8
dehuihu <dehuihu@gmail.com>
2025-04-18 07:49:43 -0700

    Implemented 02b-404 for read-one-book

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.book import books
+
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
@@ -19,13 +20,24 @@ def get_all_books():
     )
   return books_list
 
-@books_bp.get("/<book_id>")
-def get_one_book(book_id):
-  book_id = int(book_id)
+def validate_book(book_id):
+  try:
+    book_id = int(book_id)
+  except:
+    response = {"message": f"book {book_id} invalid"}
+    abort(make_response(response, 400))
   for book in books:
     if book.id == book_id:
-      return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-      }
+      return book
+  response = {"message": f"book {book_id} not found"}
+  abort(make_response(response, 404))
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+  book = validate_book(book_id) 
+
+  return {
+    "id": book.id,
+    "title": book.title,
+    "description": book.description
+  }
